87bee55b4696e7e23fc687c7b27e6a7be19d0af5
Wei Xin <cheewx@hotmail.com>
2020-06-18 13:43:22 +0800

    fix filter

--- src/PivottableUi.js
+++ src/PivottableUi.js
@@ -163,6 +163,7 @@ export default {
     },
     openFilterBox ({ attribute, open }) {
       this.openStatus[attribute] = open
+      Object.keys(this.openStatus).map(this.assignValue)
     },
     materializeInput (nextData) {
       if (this.propsData.data === nextData) {
