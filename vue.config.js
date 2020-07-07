4a973037a9131ba7f3b40df901cf834c63eb1cd3
hawjeh <hawjeh.lee@websparks.sg>
2020-07-07 15:58:24 +0800

    remove jquery ui

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,22 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   productionSourceMap: false,
-  chainWebpack: config => {
-    config.module.rules.delete("eslint");
-  },
   configureWebpack: {
     output: {
-      libraryExport: "default"
+      libraryExport: 'default',
     },
     plugins: [
       new CopyWebpackPlugin([
         {
-          from: path.join(__dirname, "src/assets"),
-          to: path.join(__dirname, "dist"),
-          toType: "dir"
-        }
+          from: path.join(__dirname, 'src/assets'),
+          to: path.join(__dirname, 'dist'),
+          toType: 'dir',
+        },
       ]),
-      new BundleAnalyzerPlugin()
-    ]
-  }
+      new BundleAnalyzerPlugin(),
+    ],
+  },
 };
