bfd9039ac86f6509e8ebb71cf3f64fa45842ed83
DHL <dhl1402@gmail.com>
2020-10-27 10:06:41 +0700

    Lazy mount raw table

--- src/Pivottable.js
+++ src/Pivottable.js
@@ -4,6 +4,12 @@ export default {
   name: 'vue-pivottable',
   props: ['tableMaxWidth'],
   mixins: [defaultProps],
+  data () {
+    return {
+      mountRawTable: false,
+      rawTableObserver: null
+    }
+  },
   computed: {
     renderers () {
       return TableRenderer[this.rendererName in TableRenderer ? this.rendererName : Object.keys(TableRenderer)[0]]
@@ -14,7 +20,7 @@ export default {
       const props = this.$props
       const scopedSlots = this.$scopedSlots
       return h(this.renderers, {
-        props,
+        props: { ...props, mountRawTable: this.mountRawTable },
         scopedSlots
       })
     },
@@ -31,34 +37,43 @@ export default {
       ])
     },
     exportXls ({ filename }) {
-      const tableHtml = `
-        <html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">
-          <head>
-            <!--[if gte mso 9]>
-            <xml>
-              <x:ExcelWorkbook>
-                <x:ExcelWorksheets>
-                  <x:ExcelWorksheet>
-                    <x:Name>Sheet 1</x:Name>
-                    <x:WorksheetOptions>
-                      <x:DisplayGridlines/>
-                    </x:WorksheetOptions>
-                  </x:ExcelWorksheet>
-                </x:ExcelWorksheets>
-              </x:ExcelWorkbook></xml>
-            <![endif]-->
-            <meta http-equiv="content-type" content="text/plain; charset=UTF-8"/>
-          </head>
-          <body>
-            ${document.getElementById(`pivottable${this.$vnode.key}`).outerHTML}
-          </body>
-        </html>
-      `
-      const downloadLink = document.createElement('a')
-      document.body.appendChild(downloadLink)
-      downloadLink.href = `data:application/vnd.ms-excel;base64,${window.btoa(unescape(encodeURIComponent(tableHtml)))}`
-      downloadLink.download = filename || 'pivot_data.xls'
-      downloadLink.click()
+      this.mountRawTable = true
+      const rawTableId = `pivottable${this.$vnode.key}`
+      this.rawTableObserver = new MutationObserver(() => {
+        if (document.getElementById(rawTableId)) {
+          const tableHtml = `
+            <html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">
+              <head>
+                <!--[if gte mso 9]>
+                <xml>
+                  <x:ExcelWorkbook>
+                    <x:ExcelWorksheets>
+                      <x:ExcelWorksheet>
+                        <x:Name>Sheet 1</x:Name>
+                        <x:WorksheetOptions>
+                          <x:DisplayGridlines/>
+                        </x:WorksheetOptions>
+                      </x:ExcelWorksheet>
+                    </x:ExcelWorksheets>
+                  </x:ExcelWorkbook></xml>
+                <![endif]-->
+                <meta http-equiv="content-type" content="text/plain; charset=UTF-8"/>
+              </head>
+              <body>
+                ${document.getElementById(rawTableId).outerHTML}
+              </body>
+            </html>
+          `
+          const downloadLink = document.createElement('a')
+          document.body.appendChild(downloadLink)
+          downloadLink.href = `data:application/vnd.ms-excel;base64,${window.btoa(unescape(encodeURIComponent(tableHtml)))}`
+          downloadLink.download = filename || 'pivot_data.xls'
+          downloadLink.click()
+          this.mountRawTable = false
+          this.rawTableObserver.disconnect()
+        }
+      })
+      this.rawTableObserver.observe(document.body, { childList: true, subtree: true })
     }
   },
   render (h) {
