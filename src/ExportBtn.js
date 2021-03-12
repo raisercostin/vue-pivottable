ac098b365d1ff43499ac841e8e0a356927b85e7d
hawjeh.lee <hawjeh.lee@WS-HAWJEH>
2021-03-12 11:28:40 +0800

    remove unuse code. update to use other excel export plugin

--- src/ExportBtn.js
+++ src/ExportBtn.js
@@ -1,7 +1,4 @@
-import * as XLSX from 'xlsx';
-import * as html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
+import TableToExcel from "@linways/table-to-excel";
 import $ from 'jquery';
 
 export default {
@@ -15,7 +12,7 @@ export default {
   },
   methods: {
     exportFile(format) {
-      exportDocument('.pvtOutput', 'table.pvtTable', format);
+      exportDocument('table.pvtTable', format);
     },
   },
   render(h) {
@@ -39,97 +36,23 @@ export default {
             },
           }
         ),
-          h(
-            "span",
-            'Export to Excel'
-          )
+        h(
+          "span",
+          'Export to Excel'
+        )
         ]
-      ),
-      // h(
-      //   'div',
-      //   {
-      //     staticClass: ['OuterExportFilterBox'],
-      //   },
-      //   [
-      //     h(
-      //       'button',
-      //       {
-      //         staticClass: ['greenBtn'],
-      //         on: {
-      //           click: () => this.exportFile('xlsx'),
-      //         },
-      //       },
-      //       `Export XLSX`
-      //     ),
-      //     h(
-      //       'button',
-      //       {
-      //         staticClass: ['greenBtn'],
-      //         on: {
-      //           click: () => this.exportFile('pdf'),
-      //         },
-      //       },
-      //       `Export PDF`
-      //     ),
-      //     h(
-      //       'button',
-      //       {
-      //         staticClass: ['greenBtn'],
-      //         on: {
-      //           click: () => this.exportFile('png'),
-      //         },
-      //       },
-      //       `Export PNG`
-      //     ),
-      //   ]
-      // ),
+      )
     ]);
   },
 };
 
-function exportDocument(parent, className, format) {
+function exportDocument(className, format) {
   format = format.toLowerCase();
-  var fileName = 'Export';
-  var table = $(className)[0];
-  var parent = $(parent)[0];
   $(className).attr('border', '1');
+  var table = $(className)[0];
 
   if (format === 'xlsx') {
-    var wb = XLSX.utils.table_to_book(table, { sheet: 'Sheet 1' });
-    return XLSX.writeFile(wb, fileName + '.xlsx');
-  } else if (format === 'png') {
-    window.scrollTo(0, 0);
-    html2canvas(parent, {
-      allowTaint: true,
-      scrollY: -window.scrollY,
-      scrollX: -window.scrollX
-    }).then(function (canvas) {
-      var donwloadLink = document.createElement('a');
-      donwloadLink.href = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
-      donwloadLink.download = fileName + '.png';
-      donwloadLink.target = '_blank';
-      donwloadLink.style.display = 'none';
-
-      document.body.appendChild(donwloadLink);
-      donwloadLink.click();
-      document.body.removeChild(donwloadLink);
-
-      $(className).removeAttr('style');
-    });
-  } else if (format === 'pdf') {
-    var doc = new jsPDF({ orientation: 'landscape' });
-    doc.autoTable({
-      theme: 'plain',
-      styles: { halign: 'center', fillColor: [0, 131, 87], textColor: [0, 0, 0], lineWidth: 0.01, lineColor: [235, 240, 248] },
-      headStyles: { halign: 'center', textColor: [255, 255, 255], fontStyle: 'normal' },
-      bodyStyles: { halign: 'right', fillColor: [255, 255, 255], textColor: [0, 0, 0] },
-      columnStyles: { text: { cellWidth: 'wrap' }, 0: { halign: 'right', fillColor: [0, 131, 87], textColor: [255, 255, 255] } },
-      margin: { top: 5, bottom: 5, left: 5, right: 5 },
-      tableLineWidth: 0.01,
-      tableLineColor: [235, 240, 248],
-      html: table,
-    });
-    doc.save(fileName + '.pdf');
+    return TableToExcel.convert(table);;
   }
 }
 
